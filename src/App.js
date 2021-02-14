8a10f023ecc8f46d44eeea5ce8a8af3597e724fd
Fredrik <fredrikf2000@gmail.com>
2021-02-14 20:12:41 +0100

    added separate component for fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 import './App.css';
 
 
@@ -13,12 +14,16 @@ const App = () => {
       const result = await axios(`https://www.breakingbadapi.com/api/characters`);
 
       console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
     }
+    fetchItems();
   })
 
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
