930b4c839c3e5997432aed2474b022eb20a5a471
Fredrik <fredrikf2000@gmail.com>
2021-02-14 20:31:12 +0100

    setting query so users can filter through data

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import './App.css';
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
 
       console.log(result.data);
       setItems(result.data);
@@ -24,7 +25,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 
-const Search = () => {
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState('');
+    const onChange = (q) => {
+        setText(q);
+        getQuery(q);
+    }
+
     return (
         <section className='search'>
             <form>
-                <input type='text' className='form-control' placeholder='Search for characters' />
+                <input type='text' className='form-control' placeholder='Search for characters' value={text} onChange={(e) => onChange(e.target.value)} autoFocus />
             </form>
         </section>
     )
