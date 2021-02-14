5dda40b0cd8525b664af0b5278b4d5e799092c94
Fredrik <fredrikf2000@gmail.com>
2021-02-14 20:15:25 +0100

    testing that fetch outputs content

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const CharacterGrid = () => {
-    return (
-        <div>
-
-        </div>
-    )
+const CharacterGrid = ({ items, isLoading }) => {
+    return isLoading ? <h1>Loading...</h1> : <section className='cards'>
+        {items.map(item => (
+            <h1>{item.name}</h1>
+        ))}
+    </section>
 }
 
 export default CharacterGrid
